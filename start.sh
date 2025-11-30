8a8c19cafac7f9c8ca0c3aba46269b5905d7c0ed
relatoscontosnoturnos <relatoscontosnoturnos@gmail.com>
2025-11-30 18:19:16 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Inicia o PHP-FPM em background
-echo "Iniciando PHP-FPM..."
-/usr/local/sbin/php-fpm &
+php-fpm -D
 
-# Inicia o Caddy em foreground (mantendo o container ativo)
-echo "Iniciando Caddy..."
-exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
